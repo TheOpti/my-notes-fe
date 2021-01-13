b29f028cd17600475643b918aa32bccc5847a381
TheOpti <wieckowski.piotr.93@gmail.com>
2021-01-13 22:39:41 +0100

    MN-4 Upgrade test to run

--- setupTests.ts
+++ setupTests.ts
@@ -1,4 +1,6 @@
 import enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+// import Adapter from 'enzyme-adapter-react-16';
+// TODO: Enzyme is not yet compatible with React v.17
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 enzyme.configure({ adapter: new Adapter() });
